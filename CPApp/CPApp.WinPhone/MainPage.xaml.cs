107feaebebbc7623c3a248367e612e04ded0a98a
laureysruben <RL35461a>
2014-06-04 16:51:58 +0200

    first commit

---
+++ CPApp/CPApp.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace CPApp.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = CPApp.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
---
+++ CPApp/CPApp/App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CPApp
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new ContentPage
+            {
+                Title = "Fluo's movies",
+                Content = new StackLayout
+                {
+                    Spacing = 20,
+                    Padding= 50,
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new ListView { 
+                            ItemsSource = new String[] {
+                                "Game of Thrones",
+                                "Walking Dead",
+                                "Dexter",
+                                "True Blood"
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
